875659d8fddad39671f955315e1f4080ac746753
pra152 <pravatdas211@gmail.com>
2021-07-12 08:35:55 +0530

    Added:Arithmatic Computation

--- Arithmatic_Computation_And_Sorting_Problem.sh
+++ Arithmatic_Computation_And_Sorting_Problem.sh
@@ -2,3 +2,7 @@ echo Welcomes to Arithmatic Computation and Sorting
 read -p "Enter first input :" a
 read -p "Enter second input:" b
 read -p "Enter third input :" c
+
+result=$(($a + $b * $c))
+echo "Result:" $result
+
